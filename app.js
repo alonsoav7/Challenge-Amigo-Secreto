a7751898ac9a6a2328ecbcfd029b88149ebd158d
Alonso Aguilar <alonso.aguilar.v@gmail.com>
2025-08-14 23:05:22 -0600

    Agregando funcionalidad de botón añadir amigo

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Crear un array para almacenar los nombres
+let nombresIngresados = [];
+
+//Función para agregar amigos
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById('amigo').value;
+
+    if (nombreAmigo == "") {
+        console.log("Escriba un nombre nombre válido");
+    }else {
+        nombresIngresados.push(nombreAmigo);
+    }
+    limpiarCaja();
+    console.log(nombresIngresados);
+
+}
+
+function limpiarCaja () {
+   document.querySelector('#amigo').value = '';
+   
+}
